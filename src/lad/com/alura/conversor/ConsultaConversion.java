28a39188ed0d4898cd3212c5059c1396d4ba22f8
Angelo270204 <angelow.piscis.27@gmail.com>
2025-11-26 23:13:54 -0500

    Clase que realizara la peticion a la API agregada

--- src/lad/com/alura/conversor/ConsultaConversion.java
+++ src/lad/com/alura/conversor/ConsultaConversion.java
@@ -1,4 +1,26 @@
 package lad.com.alura.conversor;
 
-public class ConsultaConversion {
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaConversion{
+    private static final HttpClient client = HttpClient.newHttpClient();
+    private static final Gson gson = new Gson();
+
+    public RespuestaPeticion obtenerDatos() throws IOException, InterruptedException {
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/e83be608fe711cce4725dd9d/latest/USD");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+        return gson.fromJson(json, RespuestaPeticion.class);
+    }
 }
