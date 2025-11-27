e1b9c842ec8aec975ec8188b01ccd8829dd9d007
Angelo270204 <angelow.piscis.27@gmail.com>
2025-11-26 23:11:54 -0500

    Creacion de la interfaz que se mostrara por consola

--- src/lad/com/alura/conversor/InterfazConsola.java
+++ src/lad/com/alura/conversor/InterfazConsola.java
@@ -1,4 +1,20 @@
 package lad.com.alura.conversor;
 
 public class InterfazConsola {
+    public static void exhibirMenu() {
+        System.out.println("""
+                ********************************************************************
+                Sea bienvenido/a al Conversor de Moneda =]
+                
+                1) Dolar =>> Peso Argentino
+                2) Peso Argentino =>> Dolar
+                3) Dolar =>> Real brasileno
+                4) Real brasileno =>> Dolar
+                5) Dolar =>> Peso colombiano
+                6) Peso Colombiano =>> Dolar
+                7) Salir
+                Elija una opcion valida:
+                ********************************************************************
+                """);
+    }
 }
